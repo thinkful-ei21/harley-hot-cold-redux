9a1f7fd6ea6a5cf754aadd442a4fe2ce34fb47c2
giovanni <mormagli@gmail.com>
2018-06-22 11:28:19 -0500

    wrote redux skeleton

---
+++ src/actions/actions.js
@@ -0,0 +1,20 @@
+
+export ADD_GUESS = 'ADD_GUESS'
+export const addGuess = (guess) =>{
+    return {
+        type: ADD_GUESS,
+        guess
+    }
+}
+
+export const clearGuesses = ()=>{
+
+}
+
+export const toggleOverlay =() =>{
+
+}
+
+export const resetGame = () =>{
+
+}
---
+++ src/reducers/gameReducer.js
@@ -0,0 +1,8 @@
+import * as actions from '../actions/actions';
+
+const initialState = {};
+
+export const gameReducer = (state=initialState, action) =>{
+
+    return state
+}
